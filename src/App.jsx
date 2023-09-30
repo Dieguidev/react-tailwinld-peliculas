9dd36a0164f448914b8b1db1995d62e45dc38377
Dieguidev <diegogaraycullas@gmail.com>
2023-09-30 09:26:01 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <header>
+      <h1 className='text-center text-5xl text-[#9F91FE] font-bold'>
+        📽️Películas
+      </h1>
+    </header>
+  )
+}
+
+export default App
